886c200346f8cfedc5af191f036708a1e14d2e2c
zakiabdessem <zakiabdessemed35@gmail.com>
2021-05-21 17:53:14 +0100

    Update javasc.js

--- Projet Ex1/JavaScript/javasc.js
+++ Projet Ex1/JavaScript/javasc.js
@@ -6,3 +6,19 @@ document.getElementById(K).write("I am feeling "+ Felling )
 else{
 document.getElementById(K).write("I not am feeling good")
 }
+
+//Wla code 2 eme hada bsh mem hwa mabgach ymchi 
+
+/*
+ var Felling =prompt("what is your Felling Today");
+var Good;
+var Great;
+var Fine;
+
+if(Felling = "Good","Great","Fine"){
+document.getElementById(K).write("I am feeling "+ Felling )
+}
+else{
+document.getElementById(K).write("I not am feeling good")
+}
+/*

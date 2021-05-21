4b7b5592516d8ceb521016280766d9588e231440
zakiabdessem <zakiabdessemed35@gmail.com>
2021-05-21 17:43:27 +0100

    //Ex1

---
+++ Projet Ex1/JavaScript/javasc.js
@@ -0,0 +1,8 @@
+var Felling =prompt("what is your Felling Today");
+Object(Good,Great,Fine)
+if(Felling = Object){
+document.getElementById(K).write("I am feeling "+ Felling )
+}
+else{
+document.getElementById(K).write("I not am feeling good")
+}
